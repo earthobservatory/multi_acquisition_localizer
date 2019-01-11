406c7f2c892d5b2d0a0d2c766166045989a3ef35
Mohammed Karim <mkarom@jpl.nasa.gov>
2019-01-11 06:08:08 +0000

    temp asf hardwiring

--- acquisition_localizer_single.py
+++ acquisition_localizer_single.py
@@ -246,6 +246,9 @@ def resolve_s1_slc(identifier, download_url, asf_queue, esa_queue):
     else:
         url = download_url
         queue = esa_queue
+
+    url = r.url
+    queue = asf_queue
         #raise RuntimeError("Got status code {} from {}: {}".format(r.status_code, vertex_url, r.url))
     return url, queue
 

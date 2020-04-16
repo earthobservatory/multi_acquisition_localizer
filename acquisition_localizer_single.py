af1bc6101feb97e2b8de98ebf9daec76ad2906ec
shitong01 <stchin@ntu.edu.sg>
2020-04-16 16:36:20 +0800

    Improve this

--- acquisition_localizer_single.py
+++ acquisition_localizer_single.py
@@ -245,11 +245,12 @@ def resolve_s1_slc(identifier, download_url, asf_queue, esa_queue):
     #asf_queue = "spyddder-sling-extract-asf"
     #esa_queue = "spyddder-sling-extract-scihub"
 
-    # determine best url and corresponding queue
+    # determine best url and corresponding queue by getting first 100 bytes
     vertex_url = "https://datapool.asf.alaska.edu/SLC/SA/{}.zip".format(identifier)
-    r = requests.head(vertex_url, allow_redirects=True)
+    headers = {"Range": "bytes=0-100"}
+    r = requests.head(vertex_url, allow_redirects=True, headers=headers)
     logger.info("Status Code from ASF : %s" %r.status_code)
-    if asf_queue.upper() != "NA" and r.status_code in (200, 403):
+    if asf_queue.upper() != "NA" and r.status_code in (200, 206):
         url = r.url
         queue = asf_queue
         url_type = "asf"
